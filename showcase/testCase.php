cd17a757515442beea93f698de2a0ed1e7e384f3
TuurInDeVerte <walleyntuur@gmail.com>
2015-01-09 15:37:40 +0100

    START - Showcase

---
+++ showcase/testCase.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>ShowCase</title>
+		<link rel="stylesheet" type="text/css" href="css/testCase.css">
+<!-- 	
+		<link rel="stylesheet" type="text/css" href="css/normalize.css"> 
+		<script src="presentation/js/jquery-2.1.1.js"></script>
+-->		
+	</head>
+	<body>
+		<section class="content">
+			<aside>
+				<div class="sidebar"></div>
+			</aside>
+			<main>
+				<div class="text">
+					<div class="imageHolder inlineClass"><img id="broken_image" src="images/broken.jpg"></div><p class="inlineClass">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</main>
+		</section>
+	</body>
+</html>
+
