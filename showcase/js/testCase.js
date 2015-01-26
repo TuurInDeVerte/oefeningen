0caa071c3c2eb172e6eaa1d07238cd289d677ca3
Tuur <walleyntuur@gmail.com>
2015-01-26 12:42:04 +0100

    showcase - jquery & jquery ui

--- showcase/js/testCase.js
+++ showcase/js/testCase.js
@@ -6,3 +6,66 @@ $(function(){
     $('.sidebar, .main').css({ height: $(window).innerHeight() });
   });
 });
+
+// toon en verberg main containers (navigation)
+function toonVerberg(eTeTonenElementText){
+	// selecteer te tonen element
+	var eTeTonenElement = document.getElementById("main_" + eTeTonenElementText);
+
+	// verberg alle containers
+	$("#main_home").hide();
+	$("#main_about").hide();
+	$("#main_portfolio").hide();
+	$("#main_contact").hide();
+
+	// toon het te tonen element
+	$(eTeTonenElement).show();
+
+	// verberg zijn children en toon ze opnieuw (met effect)
+	// verberg
+	$(".main_bg").hide();
+	$(".mainText_h1").hide();
+	$(".mainText_p1").hide();
+
+	// toon
+		// toon achtergrond
+	$(".main_bg").show("slide", {direction: "down"}, 250);
+		// in het juiste kleur
+	setTimeout(function(){
+		if(eTeTonenElementText == "home"){
+			$("body").css('background-color', '#005BB7');
+		}
+		if(eTeTonenElementText == "about"){
+			$("body").css('background-color', '#004080');
+		}
+		if(eTeTonenElementText == "portfolio"){
+			$("body").css('background-color', '#8080C0');
+		}
+		if(eTeTonenElementText == "contact"){
+			$("body").css('background-color', '#0080FF');
+		}
+		}, 250);
+	setTimeout(function(){
+		$(".mainText_h1").show("slide", {direction: "up"}, 250);
+	}, 250);
+
+		// toon tekst p1
+	setTimeout(function(){
+		$(".mainText_p1").show("slide", {direction: "left"}, 250);
+	}, 250);
+}
+
+
+// START WINDOW ONLOAD
+window.onload = function(){
+	// show & hide main elements
+	$("#main_home").show();
+	$("#main_about").hide();
+	$("#main_portfolio").hide();
+	$("#main_contact").hide();
+
+	$("#navButton_home").click(function(){toonVerberg("home")});
+	$("#navButton_about").click(function(){toonVerberg("about")});
+	$("#navButton_portfolio").click(function(){toonVerberg("portfolio")});
+	$("#navButton_contact").click(function(){toonVerberg("contact")});
+} // END WINDOW ONLOAD
--- showcase/testCase.php
+++ showcase/testCase.php
@@ -4,8 +4,10 @@
 		<meta charset='utf-8'>
 		<title>ShowCase</title>
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="js/jquery-ui-1.11.2/jquery-ui.css">
 		<link rel="stylesheet" type="text/css" href="css/testCase.css">
 		<script src="js/jquery-2.1.3.js"></script>
+		<script src="js/jquery-ui-1.11.2/jquery-ui.js"></script>
 		<script src="js/testCase.js"></script>
 <!--
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
@@ -14,15 +16,50 @@
 	</head>
 	<body>
 		<section class="content">
+
 			<aside class="sidebar" id="sidebar1">
 				<div class="navButton" id="navButton_home">			<div class="navButton_content">Home		</div></div>
 				<div class="navButton" id="navButton_about">		<div class="navButton_content">About 	</div></div>
 				<div class="navButton" id="navButton_portfolio">	<div class="navButton_content">Portfolio</div></div>
 				<div class="navButton" id="navButton_contact">		<div class="navButton_content">Contact	</div></div>
 			</aside>
-			<main class="main" id="main1">
-				<p>MAIN - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+			<main class="main" id="main_home">
+				<div class="main_bg" id="main_bg_home">
+					<div class="mainText">
+						<h1 class="mainText_h1">Home</h1>
+						<p class="mainText_p1">HOME - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
 			</main>
+
+			<main class="main" id="main_about">
+				<div class="main_bg" id="main_bg_about">
+					<div class="mainText">
+						<h1 class="mainText_h1">About</h1>
+						<p class="mainText_p1">ABOUT - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</main>
+
+			<main class="main" id="main_portfolio">
+				<div class="main_bg" id="main_bg_portfolio">
+					<div class="mainText">
+						<h1 class="mainText_h1">Portfolio</h1>
+						<p class="mainText_p1">PORTFOLIO - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</main>
+
+			<main class="main" id="main_contact">
+				<div class="main_bg" id="main_bg_contact">
+					<div class="mainText">
+						<h1 class="mainText_h1">Contact</h1>
+						<p class="mainText_p1">CONTACT - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</main>
+
 		</section>
 	</body>
 </html>
